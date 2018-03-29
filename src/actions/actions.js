7986bc25633a46ab16f565bc0bf1f70a3f90385e
AGCB <oooalexoooo@gmail.com>
2018-03-29 15:53:53 -0700

    Update actions.js

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,7 +1,7 @@
 
 // add action type constants 
 // i.e.export const ACTION_DESCRIPTION = "ACTION_DESCRIPTION";
-import * as types from '../constants/actionTypes'
+import * as types from '../constants/actionTypes';
 
 const updateRating = (num) => ({
   type: types.UPDATE_RATING,
