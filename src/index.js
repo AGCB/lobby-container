dc61f7186f7e0a1639c743eb2ce4e83a15a8b11a
AGCB <oooalexoooo@gmail.com>
2018-03-29 15:55:09 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
-import App from './containers/App.js'
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import App from './containers/App.js';
 import rootReducer from './reducers/rootReducer'
 import store from "./store";
 
